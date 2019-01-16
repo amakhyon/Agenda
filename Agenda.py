c21fd45554f8d346e2dfe38a8610052d13c55b30
amakhyon <a.m.makhyon@gmail.com>
2019-01-16 14:59:14 +0200

    quotes are read from a text file

--- Agenda.py
+++ Agenda.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import random
 from PyQt5.QtWidgets import *
@@ -24,48 +25,63 @@ class Window(QWidget):
 		self.dailyTasks_label = QLabel('Tasks')
 		self.dailyTasks_label.setFont(QtGui.QFont('Sanserif',20))
 		self.addTask_lineEdit = QLineEdit('add task')
-		self.addTask_lineEdit.returnPressed.connect(self.typed)
+		self.addTask_lineEdit.returnPressed.connect(self.add_task)
 		self.encourage_label = QLabel(self)
 		self.groupBox = QGroupBox('Tasks')
 		self.tasksHbox = QHBoxLayout()
-		self.leftLayout = QVBoxLayout()
-		self.quotes = ['It doesn’t matter how slow you go as long as you don’t stop','And you ask ‘What if I fall?’ Oh but my darling, what if you fly?',
-		'Fall seven times, stand up eight','It is not the mountain we conquer but ourselves','Trust yourself. You know more than you think you do','Screw it, let’s do it',
-		'Keep going. Be all in','If you want it, work for it','You can if you think you can','Do it with passion or not at all.','At the end of hardship comes happiness.']
+		self.rightLayout = QVBoxLayout()
+		self.quotes = self.set_qoutes()
+		self.get_tasks_btn = QPushButton('get tasks',self)
+		self.get_tasks_btn.clicked.connect(self.import_tasks)
+
 		
 
 	def LayoutPosition(self):
-		self.groupBox.setLayout(self.leftLayout)
+		self.groupBox.setLayout(self.rightLayout)
 		self.centerLayout.addWidget(self.dailyTasks_label)
 		self.centerLayout.addWidget(self.addTask_lineEdit)
+		self.centerLayout.addWidget(self.get_tasks_btn)
 		self.centerLayout.addWidget(self.encourage_label)
-		#self.mainLayout.addStretch(1)
 		self.mainLayout.addLayout(self.centerLayout)
 		self.mainLayout.addWidget(self.groupBox)
 		self.setLayout(self.mainLayout)
 
 
-	def typed(self):
+	def add_task(self):
 		
 		label = QLabel(self.addTask_lineEdit.text())
 		self.addTask_lineEdit.setText('')
 		btn = QPushButton(self)
 		btn.setIcon(QtGui.QIcon('close.png'))
-		btn.clicked.connect(lambda: self.remove(hbox,label,btn))
+		btn.clicked.connect(lambda: self.remove(label,btn))
 		hbox = QHBoxLayout()
 		hbox.addWidget(label)
 		hbox.addWidget(btn)
-		self.leftLayout.addLayout(hbox)
-		
+		self.rightLayout.addLayout(hbox)
 		print('yes')
 		
 
-	def remove(self,hbox,label,btn):
+	def remove(self,label,btn):
 		index = random.randint(0,len(self.quotes)-1)
 		self.encourage_label.setText(self.quotes[index])
+		print(label.text())
 		label.hide()
 		btn.hide()
-		
+
+	def import_tasks(self):
+		for file in os.listdir():
+			if os.path.isfile(file):
+				self.addTask_lineEdit.setText(file)
+				self.add_task()
+	def set_qoutes(self):
+		if os.path.isfile('quotes.txt'):
+			with open('quotes.txt','r') as file:
+				quotes = []
+				for line in file:
+					quotes.append(line)
+			print(quotes)
+			return quotes
+
 
 
 app = QApplication(sys.argv)
